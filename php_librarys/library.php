e9d6f47f85f82c47db48ea37fe3194638c478acc
alexisescobales <99324319+alexisescobales@users.noreply.github.com>
2022-09-27 12:04:22 +0200

    Add files via upload

---
+++ php_librarys/library.php
@@ -0,0 +1,36 @@
+<?php
+
+function createPokemon ($numero, $nombre, $region, $tipo, $altura, $peso, $evolucion ,$imagen){
+    $pokemon = [
+        'Numero' => $numero,
+        'Nombre' => $nombre,
+        'Region' => $region,
+        'Tipo' => $tipo,
+        'Altura' => $altura,
+        'Peso' => $peso,
+        'Evolucion' => $evolucion,
+        'Imagen' => $imagen
+    ];
+    return $pokemon;
+}  
+
+function showPokemon ($pokemon){
+    for ($i=0; $i < count($pokemon); $i++) { 
+        echo $pokemon[$i];
+    }
+} 
+
+/*
+--Pimer pokemon informacion--
+'Numero' => 001,
+'Nombre' => "Bulbasur",
+'Region' => "Kanto",
+'Tipo' => "Planta/Veneno",
+'Altura' => 0.7,
+'Peso' => 6.9,
+'Evolucion' => "Primera",
+'Imagen' => "./users/001.png"
+*/
+?>
+
+
