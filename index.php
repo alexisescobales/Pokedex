826f862ef9ea4c51eb5a9e96916ba1bf3e0791f7
Alexis Escobales <aescobalesg2122@politecnics.barcelona>
2022-09-29 13:57:32 +0200

    aa

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
 </head>
 <body>
 
-<h1>This is a Heading</h1>
+<h1>This is a Headin</h1>
+
 <?php
 
 createPokemon(001,"Bulbasur","Kanto","Planta/Veneno",0.7,6.9,"Primera","./media/001.png");
-showPokemon($pokemon);
+$pokemon = createPokemon(001,"Bulbasur","Kanto","Planta/Veneno",0.7,6.9,"Primera","./media/001.png");
+
+for ($i=0; $i < count($pokemon); $i++) { 
+    echo $pokemon[$i];
+}
 ?>
 
 </body>
