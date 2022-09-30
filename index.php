7fd9ac96a50afb53dfe4370fd95870846fcb831b
alexisescobales <99324319+alexisescobales@users.noreply.github.com>
2022-09-30 13:46:40 +0200

    Funciones realizadas 1,2,3

--- index.php
+++ index.php
@@ -9,21 +9,34 @@ include ('./php_librarys/library.php'); ?>
 <body>
 
 <?php
+$pokedex = array();
 
+//----------------------------------------CREACION DE POKEMONS------------------------------------------//
 $pokemon1 = createPokemon(001,"Bulbasur","Hoen","Veneno/Planta",70,6.9,"Sin Evolucion","./media/001.png");
 $pokemon2 = createPokemon(002,"Ivysaur","Hoen","Veneno/Planta",100,13,"Primera Evolucion","./media/002.png");
 $pokemon3 = createPokemon(004,"Charmander","Jotho","Fuego",60,8.5,"Sin Evolucion","./media/003.png");
+//----------------------------------------CREACION DE POKEMONS------------------------------------------//
+
+
+
+//----------------------------------------MOSTRAR POKEMONS------------------------------------------//
 showPokemon($pokemon1);
 echo "</br>";
 showPokemon($pokemon2);
 echo "</br>";
 showPokemon($pokemon3);
+//----------------------------------------MOSTRAR POKEMONS------------------------------------------//
 
 
-addPokedex($pokemon1);
-addPokedex($pokemon2);
-addPokedex($pokemon3);
 
+
+
+//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
+$pokedex = addPokedex($pokedex,$pokemon1);
+$pokedex = addPokedex($pokedex,$pokemon2);
+$pokedex = addPokedex($pokedex,$pokemon3);
+print_r($pokedex);
+//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
 
 ?>
 
