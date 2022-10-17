d13e5d374863c4a6b20b3a1d85512cd1453d80ee
Alexis Escobales <aescobalesg2122@politecnics.barcelona>
2022-10-17 12:08:31 +0200

    boostrap navbar. Falta backround pokedex.png

--- index.php
+++ index.php
@@ -3,52 +3,40 @@ include ('./php_librarys/library.php'); ?>
 <?php include './style/css_index.css'; ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+    <php></php>
 <head>
-<title>Pokedex</title>
-<link rel="stylesheet" href="./bootstrap-5.2.2-dist/css/bootstrap.min.css">
-<script src="./bootstrap-5.2.2-dist/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <script src="./bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class="background">
-
-<?php
-$pokedex = array();
-
-//----------------------------------------CREACION DE POKEMONS------------------------------------------//
-$pokemon1 = createPokemon('001',"Bulbasur","Hoen","Veneno/Planta",'70','6.9',"Sin Evolucion","./media/001.png");
-$pokemon2 = createPokemon('002',"Ivysaur","Hoen","Veneno/Planta",'100','13',"Primera Evolucion","./media/002.png");
-$pokemon3 = createPokemon('004',"Charmander","Jotho","Fuego",'60','8.5',"Sin Evolucion","./media/003.png");
-//----------------------------------------CREACION DE POKEMONS------------------------------------------//
-
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-$pokedex = addPokedex($pokedex,$pokemon1);
-$pokedex = addPokedex($pokedex,$pokemon2);
-$pokedex = addPokedex($pokedex,$pokemon3);
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-showPokedex($pokedex);
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-
-//----------------------------------------ELIMINAR POKEMON DE POKEDEX------------------------------------------//
-
-//----------------------------------------ELIMINAR POKEMON DE POKEDEX------------------------------------------//
-
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-
-//----------------------------------------AÑADIR POKEMON A POKEDEX------------------------------------------//
-
-
-
-//----------------------------------------MOSTRAR POKEMONS------------------------------------------//
-showPokemon($pokemon1);
-echo "</br>";
-showPokemon($pokemon2);
-echo "</br>";
-showPokemon($pokemon3);
-//----------------------------------------MOSTRAR POKEMONS------------------------------------------//
-
-?>
-
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+         <a class="navbar-brand" href="#">
+         <img src="./media/pokedex.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+         Pokedex
+    </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Datos Maestros
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./php_views/pokemon_list.php">Pokemons</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </body>
 </html>
